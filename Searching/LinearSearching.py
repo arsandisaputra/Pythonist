0ba16d6efc52ea616bd0d0d87fc021021ccbc4af
Arsandi Saputra <71245230+arsandisaputra@users.noreply.github.com>
2021-09-30 08:00:11 +0700

    Add files via upload

---
+++ Searching/LinearSearching.py
@@ -0,0 +1,12 @@
+elmnts = [3, 9, 8, 2, 1, 4, 6, 5, 7]
+search = -1
+found = False
+index = 0
+while index < len(elmnts):
+    if search == elmnts[index]:
+        found = True
+        print('Find', search, 'in index', index)
+        break
+    index += 1
+if not found:
+    print(search, 'not founded!')
